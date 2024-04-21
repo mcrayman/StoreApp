81cdd47f41e3803a1904eab7beed59d436aab3ba
Matt McMeans <mrmcmans@memphis.edu>
2024-04-21 18:31:15 -0500

    create views

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Products
+from django.shortcuts import get_object_or_404
 
 def home(request):
   return render(request, 'pages/home.html')
+
+def products_list(request):
+  products = Products.objects.all()
+  return render(request, 'products/index.html', {'products': products})
+
+def product_detail(request, id):
+  product = get_object_or_404(Products, id=id)
+  return render(request, 'products/show.html', {'product': product})
